d681124e8921c236807bfec08c85859b380d6e09
wardalbam <wardalbam32@gmail.com>
2020-06-16 18:40:39 +0200

    first commit....

---
+++ src/git_test/controller.java
@@ -0,0 +1,17 @@
+package git_test;
+
+public class controller {
+	public String test_string;
+
+	public controller(String test_string) {
+		super();
+		this.test_string = test_string;
+	}
+	
+	public String getTestString() {
+		
+		return this.test_string;
+	}
+	
+	
+}
---
+++ src/git_test/main.java
@@ -0,0 +1,14 @@
+package git_test;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		controller test = new controller("this is a test");
+		System.out.println(test.getTestString());
+		
+		
+	}
+
+}
